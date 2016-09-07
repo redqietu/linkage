5e9b02b7f21b7cc894c9829d57fbc29e653c8164
wanghongxin492 <wanghongxin492@emao.com>
2016-09-07 11:07:37 +0800

    添加默认值

--- linkage.js
+++ linkage.js
@@ -1,17 +1,23 @@
 //require underscore and jquery
-var linkage2level = function() {
+var select2 = function() {
     var option = _.template('<option value="<%=value%>"><%=text%></option>');
-    return function select2(data, s1, s2) {
+
+    return function select2(data, s1, s2, defaultValue, defaultValue2) {
         var x1 = _.reduce(data, function(s, x, i, l) {
             return s + option(x);
         }, '');
         var $s2 = $(s2);
-        $(s1).html(x1).on('change', function() {
+        var $s1 = $(s1).html(x1).on('change', function() {
             var city = data[$(this).val()].city;
             var x2 = _.reduce(city, function(s, x, i, l) {
                 return s + option(x);
             }, '');
-            $s2.html(x2)
-        });
+            $s2.html(x2);
+        }).val(defaultValue);
+        var city = data[$s1.val()].city;
+        var x2 = _.reduce(city, function(s, x, i, l) {
+            return s + option(x);
+        }, '');
+        $s2.html(x2).val(defaultValue2 + '');
     }
 }();
